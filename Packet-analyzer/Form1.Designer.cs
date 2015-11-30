3c1fa88f6f8df1789aae8b8ac4ede2ea28dab40f
Dalukar <dalukar42@gmail.com>
2015-11-30 21:19:42 +1000

    test capture

---
+++ Packet-analyzer/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Packet_analyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.devicesList = new System.Windows.Forms.ComboBox();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // devicesList
+            // 
+            this.devicesList.FormattingEnabled = true;
+            this.devicesList.Location = new System.Drawing.Point(12, 12);
+            this.devicesList.Name = "devicesList";
+            this.devicesList.Size = new System.Drawing.Size(1177, 21);
+            this.devicesList.TabIndex = 0;
+            // 
+            // logBox
+            // 
+            this.logBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.logBox.Location = new System.Drawing.Point(12, 105);
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.Size = new System.Drawing.Size(1208, 381);
+            this.logBox.TabIndex = 1;
+            this.logBox.Text = "";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 39);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(164, 36);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start scaner";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1225, 498);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.devicesList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox devicesList;
+        private System.Windows.Forms.RichTextBox logBox;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ Packet-analyzer/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpPcap;
+using System.Threading;
+
+namespace Packet_analyzer
+{
+    public partial class Form1 : Form
+    {
+        public Thread captureThread;
+        CaptureDeviceList devices;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string ver = SharpPcap.Version.VersionString;
+            /* Print SharpPcap version */
+            SafeLog("SharpPcap " + ver);
+            SafeLog("");
+
+            /* Retrieve the device list */
+            devices = CaptureDeviceList.Instance;
+            devicesList.DataSource = devices;
+            /*If no device exists, print error */
+            if (devices.Count < 1)
+            {
+                SafeLog("No device found on this machine");
+                return;
+            }
+ 
+        }
+
+        private void StartCapture(ICaptureDevice device)
+        {
+            device.Capture();
+        }
+
+        private  void device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            var time = e.Packet.Timeval.Date;
+            var len = e.Packet.Data.Length;
+
+            var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+
+            var tcpPacket = PacketDotNet.TcpPacket.GetEncapsulated(packet);
+            if (tcpPacket != null)
+            {
+                var ipPacket = (PacketDotNet.IpPacket)tcpPacket.ParentPacket;
+                System.Net.IPAddress srcIp = ipPacket.SourceAddress;
+                System.Net.IPAddress dstIp = ipPacket.DestinationAddress;
+                int srcPort = tcpPacket.SourcePort;
+                int dstPort = tcpPacket.DestinationPort;
+
+                SafeLog(time.Hour + ":" + time.Minute + ":" + time.Second + "," + time.Millisecond + " Len=" + len + " " +
+                    srcIp + ":" + srcPort + " -> " + dstIp + ":" + dstPort);
+            }
+        }
+
+        private  void SafeLog(string text)
+        {
+            Action chTxt = new Action(() =>
+            {
+                logBox.AppendText(text + "\n");
+                logBox.ScrollToCaret();
+            });
+            if (InvokeRequired)
+                this.BeginInvoke(chTxt);
+            else chTxt();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (captureThread != null)
+            {
+                captureThread.Abort();
+            }
+                 
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            var device = devices[devicesList.SelectedIndex];
+
+            //Register our handler function to the 'packet arrival' event
+            device.OnPacketArrival -= 
+                new PacketArrivalEventHandler(device_OnPacketArrival); //надеюсь это работает
+            device.OnPacketArrival +=
+                new PacketArrivalEventHandler(device_OnPacketArrival);
+
+            // Open the device for capturing
+            int readTimeoutMilliseconds = 1000;
+            device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
+
+            //tcpdump filter to capture only TCP/IP packets
+            string filter = "ip and tcp";
+            device.Filter = filter;
+
+            SafeLog("");
+            SafeLog
+                ("-- The following tcpdump filter will be applied: \"" + filter + "\"");
+            SafeLog
+                ("-- Listening on " + device.Description);
+
+            // Start capture 'INFINTE' number of packets
+            if (captureThread != null)
+            {
+                captureThread.Abort();
+            }
+            captureThread = new Thread(() => StartCapture(device));
+            captureThread.Start();
+        }
+    }
+}
